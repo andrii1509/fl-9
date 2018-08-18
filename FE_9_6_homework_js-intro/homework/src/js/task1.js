76b6a596af9432abf5992809a1a5f93599641afb
Khry <khrustyk@gmail.com>
2018-08-18 17:18:26 +0300

    1ready

--- FE_9_6_homework_js-intro/homework/src/js/task1.js
+++ FE_9_6_homework_js-intro/homework/src/js/task1.js
@@ -1,1 +1,14 @@
 // Your code goes here
+let price = prompt('Enter price');
+let discount = prompt("discount %");
+if (discount < 0 && discount > 100) {
+    console.log("invalid discount");
+}else if (price < 0) {
+    console.log("Wrong price");
+} else{
+
+}
+console.log("price without discount : ", price);
+console.log("Discount : ", discount);
+console.log("Price with discount",  price - (discount / 100 * price));
+console.log("saved : ",  discount / 100 * price);
