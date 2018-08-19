0459769dbce248ab0257780b3c40e4310df09c86
Khry <khrustyk@gmail.com>
2018-08-19 17:31:25 +0300

    final

--- FE_9_6_homework_js-intro/homework/src/js/task2.js
+++ FE_9_6_homework_js-intro/homework/src/js/task2.js
@@ -1,1 +1,14 @@
 // Your code goes here
+let a = +prompt("a= ");
+let b = +prompt("b = ");
+let angle = +prompt("angle = ");
+if (a < 0 || b < 0 || angle < 0) {
+    console.log("invalid data!");
+}else{
+    let c = Math.sqrt(Math.pow(a, 2)+ Math.pow(b, 2) - 2 * a * b * Math.cos(angle));
+    console.log("c length :" + c.toFixed(2));
+    let p = a + b + c;
+    console.log("perimeter : ", p.toFixed(2));
+    let square = (a * b * Math.sin(angle))/2;
+    console.log("square : " + square.toFixed(2));
+}
